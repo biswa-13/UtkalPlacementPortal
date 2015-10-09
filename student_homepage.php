74e1d4671409d1a4e752969da42ad81ba55cd69a
biswa-13 <bichhubiswa@gmail.com>
2015-10-09 11:17:24 +0530

    Changes Made By Priya For StudentHome Page
    Changes Made By Priya For StudentHome Page

--- student_homepage.php
+++ student_homepage.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>: : Utkal Placement Portal : :</title>
+    <title>Utkal Placement Portal</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
@@ -35,9 +35,9 @@
         <!-- Logo -->
         <a href="student_homepage.php" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>UU</b></span>
+          <span class="logo-mini"><img src="images/uulogo.png" height="50" width="50"></span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Utkal University</b></span>
+          <span class="logo-lg"><img src="images/uulogo.png" height="30" width="30"><b>Utkal University</b></span>
         </a>
 
         <!-- Header Navbar: style can be found in header.less -->
@@ -297,7 +297,7 @@
               <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
           </div>
-          <!-- search form -->
+          <!-- search form .....
           <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
               <input type="text" name="q" class="form-control" placeholder="Search...">
@@ -306,23 +306,19 @@
               </span>
             </div>
           </form>
+        -->
           <!-- /.search form -->
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
             <li class="header">YOUR MENU</li>
             <li class="active treeview">
               <a href="academics.php">
-                <i class="fa fa-dashboard"></i> <span>Academics</span></i>
+                <i class="fa fa-graduation-cap"></i> <span>Academics</span></i>
               </a>
-              <!--
-              <ul class="treeview-menu">
-                <li><a href="index.html"><i class="fa fa-circle-o"></i>Academic Session 1</a></li>
-                <li class="active"><a href="index2.html"><i class="fa fa-circle-o"></i>Academic Session 2</a></li>
-              </ul> -->
             </li>
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-files-o"></i>
+                <i class="fa fa-user"></i>
                 <span>Profile</span>
               </a>
               <ul class="treeview-menu">
@@ -339,20 +335,24 @@
             </li>
             <li>
               <a href="#">
-                <i class="fa fa-envelope"></i> <span>Mailbox</span>
+                <i class="fa fa-envelope"></i> <span>Inbox</span>
                 <small class="label pull-right bg-yellow">12</small>
               </a>
             </li>
             <li>
                <a href="#">
-                <i class="fa fa-gears"></i> <span>Account Settings</span>
+                <i class="fa fa-cog"></i> <span>Account Settings</span>
               </a>
               <ul class="treeview-menu">
                 <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i>Update Profile</a></li>
                 <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i>Change Password</a></li>
               </ul>
             </li>
-            
+            <li class="active treeview">
+              <a href="academics.php">
+                <i class="fa fa-star"></i> <span>Developer's Page</span></i>
+              </a>
+            </li>            
         </section>
         <!-- /.sidebar -->
       </aside>
@@ -363,167 +363,34 @@
       </div><!-- /.content-wrapper -->
 
       <footer class="main-footer">
-        <strong>Copyright &copy; 2014-2015 <a href="#">Utkal University</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; 2015 <a href="#">PLACEMENT CELL UTKAL UNIVERSITY</a>.</strong> All rights reserved. <br>
+        <strong><a href="#">PLACEMENT PORTAL 2015</a>.<br> Developed by <a href="#">Students of Integrated MCA and MTech-CSE, Utkal University</a></strong>
       </footer>
 
       <!-- Control Sidebar -->
       <aside class="control-sidebar control-sidebar-dark">
         <!-- Create the tabs -->
+        <!--
         <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
           <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
           <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
         </ul>
-        <!-- Tab panes -->
+
+      -->
+
+        <!-- Tabl panes -->
         <div class="tab-content">
           <!-- Home tab content -->
+          
           <div class="tab-pane" id="control-sidebar-home-tab">
-            <h3 class="control-sidebar-heading">Recent Activity</h3>
-            <ul class="control-sidebar-menu">
-              <li>
-                <a href="javascript::;">
-                  <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-                  <div class="menu-info">
-                    <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-                    <p>Will be 23 on April 24th</p>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <i class="menu-icon fa fa-user bg-yellow"></i>
-                  <div class="menu-info">
-                    <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
-                    <p>New phone +1(800)555-1234</p>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
-                  <div class="menu-info">
-                    <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
-                    <p>nora@example.com</p>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <i class="menu-icon fa fa-file-code-o bg-green"></i>
-                  <div class="menu-info">
-                    <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
-                    <p>Execution time 5 seconds</p>
-                  </div>
-                </a>
-              </li>
-            </ul><!-- /.control-sidebar-menu -->
-
-            <h3 class="control-sidebar-heading">Tasks Progress</h3>
-            <ul class="control-sidebar-menu">
-              <li>
-                <a href="javascript::;">
-                  <h4 class="control-sidebar-subheading">
-                    Custom Template Design
-                    <span class="label label-danger pull-right">70%</span>
-                  </h4>
-                  <div class="progress progress-xxs">
-                    <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <h4 class="control-sidebar-subheading">
-                    Update Resume
-                    <span class="label label-success pull-right">95%</span>
-                  </h4>
-                  <div class="progress progress-xxs">
-                    <div class="progress-bar progress-bar-success" style="width: 95%"></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <h4 class="control-sidebar-subheading">
-                    Laravel Integration
-                    <span class="label label-warning pull-right">50%</span>
-                  </h4>
-                  <div class="progress progress-xxs">
-                    <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="javascript::;">
-                  <h4 class="control-sidebar-subheading">
-                    Back End Framework
-                    <span class="label label-primary pull-right">68%</span>
-                  </h4>
-                  <div class="progress progress-xxs">
-                    <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
-                  </div>
-                </a>
-              </li>
-            </ul><!-- /.control-sidebar-menu -->
+           
 
           </div><!-- /.tab-pane -->
 
           <!-- Settings tab content -->
+          <!--
           <div class="tab-pane" id="control-sidebar-settings-tab">
-            <form method="post">
-              <h3 class="control-sidebar-heading">General Settings</h3>
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Report panel usage
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-                <p>
-                  Some information about this general settings option
-                </p>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Allow mail redirect
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-                <p>
-                  Other sets of options are available
-                </p>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Expose author name in posts
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-                <p>
-                  Allow the user to show his name in blog posts
-                </p>
-              </div><!-- /.form-group -->
-
-              <h3 class="control-sidebar-heading">Chat Settings</h3>
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Show me as online
-                  <input type="checkbox" class="pull-right" checked>
-                </label>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Turn off notifications
-                  <input type="checkbox" class="pull-right">
-                </label>
-              </div><!-- /.form-group -->
-
-              <div class="form-group">
-                <label class="control-sidebar-subheading">
-                  Delete chat history
-                  <a href="javascript::;" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
-                </label>
-              </div><!-- /.form-group -->
-            </form>
+            
           </div><!-- /.tab-pane -->
         </div>
       </aside><!-- /.control-sidebar -->
